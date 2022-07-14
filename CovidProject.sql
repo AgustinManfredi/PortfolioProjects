19de3bb908107017fc0e02c658d7166107150b50
AgustinManfredi <106555756+AgustinManfredi@users.noreply.github.com>
2022-07-13 22:29:39 -0300

    Update CovidProject.sql

--- CovidProject.sql
+++ CovidProject.sql
@@ -1,7 +1,7 @@
 /*
-Covid 19 Data Exploration 
+Covid 19 Exploracion de data
 
-Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Habilidades usadas: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 */
 
@@ -11,7 +11,7 @@ Where continent is not null
 order by 3,4
 
 
--- Select Data that we are going to be starting with
+-- Seleccionando data para empezar el analisis
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths$
@@ -20,62 +20,61 @@ order by 1,2
 
 
 -- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+-- Muestra la probabilidad de muerte por Covid por país 
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths$
-Where location like '%states%'
+Where location like '%argentina%'
 and continent is not null 
 order by 1,2
 
 
 -- Total Cases vs Population
--- Shows what percentage of population infected with Covid
+-- Muestra porcentaje de población infectada con Covid
 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths$
---Where location like '%states%'
+--Where location like '%aregentina%'
 order by 1,2
 
 
--- Countries with Highest Infection Rate compared to Population
+-- Paises con la mayor tasa de contagio comparados con la población
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProject..CovidDeaths$
---Where location like '%states%'
+--Where location like '%argentina%'
 Group by Location, Population
 order by PercentPopulationInfected desc
 
 
--- Countries with Highest Death Count per Population
+-- Paises con la mayor tasa de muertes por población
 
 Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths$
---Where location like '%states%'
+--Where location like '%argentina%'
 Where continent is not null 
 Group by Location
 order by TotalDeathCount desc
 
 
 
--- BREAKING THINGS DOWN BY CONTINENT
+-- VIENDO DATOS DE CADA CONTINENTE
 
--- Showing contintents with the highest death count per population
-
+-- mostrando los continentes con la mayor cantidad de muertes
 Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths$
---Where location like '%states%'
+--Where location like '%argentina%'
 Where continent is not null 
 Group by continent
 order by TotalDeathCount desc
 
 
 
--- GLOBAL NUMBERS
+-- NUMEROS GLOBALES
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths$
---Where location like '%states%'
+--Where location like '%argentina%'
 where continent is not null 
 --Group By date
 order by 1,2
@@ -83,7 +82,7 @@ order by 1,2
 
 
 -- Total Population vs Vaccinations
--- Shows Percentage of Population that has recieved at least one Covid Vaccine
+-- Mostrando el porcentaje de la población que recibió aunquesea una vacuna del Covid
 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
@@ -96,7 +95,8 @@ where dea.continent is not null
 order by 2,3
 
 
--- Using CTE to perform Calculation on Partition By in previous query
+-- Usando CTE para calcular en la partición de la query anterior
+
 
 With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
@@ -116,7 +116,7 @@ From PopvsVac
 
 
 
--- Using Temp Table to perform Calculation on Partition By in previous query
+-- Usando Temp Table para calcular en la partición de la query anterior
 
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
@@ -146,7 +146,7 @@ From #PercentPopulationVaccinated
 
 
 
--- Creating View to store data for later visualizations
+-- Creando View para guardar data para la futura visualización
 
 Create View PercentPopulationVaccinated as 
 Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
